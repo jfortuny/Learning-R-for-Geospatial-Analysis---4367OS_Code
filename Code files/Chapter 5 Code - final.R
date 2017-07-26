cd763fc8e39dfd869a931ef3c2d36c6f86d1a0b3
jfortuny <jfortuny@jfortuny.com>
2017-07-26 13:18:32 -0400

    work so fat up to chapter 6 (not completed)

--- Code files/Chapter 5 Code - final.R
+++ Code files/Chapter 5 Code - final.R
@@ -6,6 +6,7 @@ addresses = c(
 library(ggmap)
 airports = geocode(addresses)
 airports
+class(airports)
 
 airports$name = c("Albuquerque International",
 	"Double Eagle II",
@@ -26,7 +27,7 @@ library(rgdal)
 writeOGR(airports, "./Data files", "airportsJF", "ESRI Shapefile")
 
 # read the shapefile from disk
-airportsJF = readOGR("./Data files",
+airports = readOGR("./Data files",
 	"airportsJF",
 	stringsAsFactors = FALSE)
 
@@ -60,6 +61,7 @@ dimensions(airports)
 nrow(county)
 ncol(county)
 dim(county)
+head(county@data)
 
 county$NAME_2[1:10]
 
@@ -90,9 +92,9 @@ county = county[county$TYPE_2 != "Water body", ]
 ################
 # Figure 05_01 #
 ################
-png("C:\\Data\\4367OS_05_01.png",width=5.5,height=4.5,units="in",res=300,pointsize=5)
+#png("C:\\Data\\4367OS_05_01.png",width=5.5,height=4.5,units="in",res=300,pointsize=5)
 plot(county)
-dev.off()
+#dev.off()
 
 newProj = CRS("+proj=laea +lat_0=45 +lon_0=-100
 	+x_0=0 +y_0=0 +a=6370997 +b=6370997 +units=m +no_defs")
@@ -101,23 +103,23 @@ county = spTransform(county, newProj)
 ################
 # Figure 05_02 #
 ################
-png("C:\\Data\\4367OS_05_02.png",width=5.5,height=4.5,units="in",res=300,pointsize=5)
+#png("C:\\Data\\4367OS_05_02.png",width=5.5,height=4.5,units="in",res=300,pointsize=5)
 plot(county)
-dev.off()
+#dev.off()
 
 library(raster)
-l_03 = brick("C:\\Data\\landsat_11_09_2003.tif")
+l_03 = brick("./Data files/landsat_11_09_2003.tif")
 
 track = spTransform(track, CRS(proj4string(l_03)))
 
 ################
 # Figure 05_03 #
 ################
-png("C:\\Data\\4367OS_05_03.png",width=3.95,height=5.5,units="in",res=300)
+#png("C:\\Data\\4367OS_05_03.png",width=3.95,height=5.5,units="in",res=300)
 plotRGB(l_03, r = 3, g = 2, b = 1, stretch = "lin",
 	ext = extent(track) + 10000)
 plot(track, add = TRUE, col = "yellow")
-dev.off()
+#dev.off()
 
 # Working with the geometrical properties of vector layers
 coordinates(airports)
@@ -130,6 +132,8 @@ county$area = gArea(county, byid = TRUE) / 1000^2
 head(county@data)
 
 county_nv_ut = county[county$NAME_1 %in% c("Nevada", "Utah"), ]
+head(county_nv_ut@data)
+dim(county_nv_ut)
 
 states = gUnaryUnion(county_nv_ut, id = county_nv_ut$NAME_1)
 
@@ -138,12 +142,16 @@ row.names(states)
 ################
 # Figure 05_04 #
 ################
-png("C:\\Data\\4367OS_05_04.png",width=5.5,height=5.5,units="in",res=300,pointsize=5)
+#png("C:\\Data\\4367OS_05_04.png",width=5.5,height=5.5,units="in",res=300,pointsize=5)
 plot(county_nv_ut, border = "lightgrey", lty = "dotted")
 plot(states, add = TRUE)
 county_ctr = gCentroid(county_nv_ut, byid = TRUE)
-text(county_ctr, county_nv_ut$NAME_2, cex = 1.5)
-dev.off()
+class(county_ctr@coords)
+county_ctr@coords[,1]
+
+#text(county_ctr, county_nv_ut$NAME_2, cex = 1.5)
+text(county_ctr@coords[,1], county_ctr@coords[,2], county_nv_ut$NAME_2, cex = 0.8)
+#dev.off()
 
 airports = spTransform(airports, CRS(proj4string(county)))
 
@@ -152,10 +160,10 @@ nm = county[county$NAME_1 == "New Mexico", ]
 ################
 # Figure 05_05 #
 ################
-png("C:\\Data\\4367OS_05_05.png",width=5.5,height=5.5,units="in",res=300)
+#png("C:\\Data\\4367OS_05_05.png",width=5.5,height=5.5,units="in",res=300)
 plot(nm)
 plot(airports, col = "red", pch = 16, add = TRUE)
-dev.off()
+#dev.off()
 
 over(airports, nm)
 
@@ -164,22 +172,23 @@ cbind(airports@data, over(airports, nm))
 ################
 # Figure 05_06 #
 ################
-png("C:\\Data\\4367OS_05_06.png",width=5.5,height=5.5,units="in",res=300)
+#png("C:\\Data\\4367OS_05_06.png",width=5.5,height=5.5,units="in",res=300)
 plot(nm[airports, ])
 plot(airports, add = TRUE, col = "red", pch = 16, cex = 1.5)
 text(airports, airports$name, pos = 1)
-dev.off()
+#dev.off()
 
-boundary = readOGR("C:\\Data", "CTYUA_DEC_2013_EW_BFE")
+boundary = readOGR("./Data files", "CTYUA_DEC_2013_EW_BFE")
 
 proj4string(boundary)
 
-buildings = readOGR("C:\\Data", "london_buildings")
-natural = readOGR("C:\\Data", "london_natural")
+buildings = readOGR("./Data files", "london_buildings")
+natural = readOGR("./Data files", "london_natural")
 
 proj4string(buildings)
 proj4string(natural)
 
+# Re-project into the CRS of boundary
 buildings = spTransform(buildings, CRS(proj4string(boundary)))
 natural = spTransform(natural, CRS(proj4string(boundary)))
 
@@ -200,14 +209,15 @@ river = gUnaryUnion(river)
 ################
 # Figure 05_07 #
 ################
-png("C:\\Data\\4367OS_05_07.png",width=5.5,height=5.5,units="in",res=300,pointsize=5)
+#png("C:\\Data\\4367OS_05_07.png",width=5.5,height=5.5,units="in",res=300,pointsize=5)
 plot(buildings, col = "sandybrown")
 plot(river, col = "lightblue", add = TRUE)
 plot(boundary, border = "dimgrey", add = TRUE)
-dev.off()
+#dev.off()
 
-haifa_buildings = readOGR("C:\\Data", "haifa_buildings")
-haifa_natural = readOGR("C:\\Data", "haifa_natural")
+# Creating new geometries/layers from existing layers
+haifa_buildings = readOGR("./Data files", "haifa_buildings")
+haifa_natural = readOGR("./Data files", "haifa_natural")
 
 israel_adm = getData("GADM", country = "ISR", level = 1)
 
@@ -222,11 +232,11 @@ haifa_natural =
 ################
 # Figure 05_09 #
 ################
-png("C:\\Data\\4367OS_05_09.png",width=5.5,height=5.5,units="in",res=300,pointsize=5)
+#png("C:\\Data\\4367OS_05_09.png",width=5.5,height=5.5,units="in",res=300,pointsize=5)
 plot(haifa_natural, col = "lightgreen")
 plot(haifa_buildings, add = TRUE)
 plot(haifa_adm, add = TRUE)
-dev.off()
+#dev.off()
 
 buildings_ch = gConvexHull(haifa_buildings)
 
@@ -243,13 +253,14 @@ haifa_natural = gDifference(haifa_natural, buildings_50m)
 ################
 # Figure 05_10 #
 ################
-png("C:\\Data\\4367OS_05_10.png",width=5.5,height=5.5,units="in",res=300,pointsize=5)
+#png("C:\\Data\\4367OS_05_10.png",width=5.5,height=5.5,units="in",res=300,pointsize=5)
 plot(buildings_ch, col = "lightgrey", border = "lightgrey")
 plot(haifa_adm, add = TRUE)
 plot(haifa_natural, col = "lightgreen", add = TRUE)
 plot(haifa_buildings, add = TRUE)
-dev.off()
+#dev.off()
 
+# Calculating distances between geometries
 dist = gDistance(buildings, river, byid = TRUE)
 
 class(dist)
@@ -260,7 +271,8 @@ buildings$dist_river = dist[1, ]
 
 head(buildings@data)
 
-dat = read.csv("C:\\Data\\CO-EST2012-Alldata.csv")
+# Joining geometries with tabular data
+dat = read.csv("./Data files/CO-EST2012-Alldata.csv")
 
 colnames(dat)[1:15]
 
--- Code files/Chapter 6 Code - final.R
+++ Code files/Chapter 6 Code - final.R
@@ -1,5 +1,5 @@
 ### From Chapter 3
-dates = read.csv("C:\\Data\\modis_dates.csv")
+dates = read.csv("./Data files/modis_dates.csv")
 dates$date = as.Date(
 	paste(dates$year, dates$month, dates$day, sep = "-"))
 month = c(12, 1:11)
@@ -11,13 +11,13 @@ dates = join(dates, seasons, "month")
 
 ### From Chapter 4
 library(raster)
-l_00 = brick("C:\\Data\\landsat_04_10_2000.tif")
+l_00 = brick("./Data files/landsat_04_10_2000.tif")
 ndvi = function(x) (x[4] - x[3]) / (x[4] + x[3])
 ndvi_00 = calc(l_00, fun = ndvi)
 l_rec = ndvi_00
 l_rec[l_rec <= 0.2] = 0
 l_rec[l_rec > 0.2] = 1
-r = brick("C:\\Data\\modis.tif")
+r = brick("./Data files/modis.tif")
 ###
 
 dem1 = getData("SRTM", lon=33, lat=33)
@@ -25,13 +25,13 @@ dem2 = getData("SRTM", lon=38, lat=33)
 
 dem = merge(dem1, dem2)
 
-haifa_buildings = readOGR("C:\\Data", "haifa_buildings")
+haifa_buildings = readOGR("./Data files", "haifa_buildings")
 haifa_surrounding = extent(haifa_buildings) + 0.25
 
 ################
 # Figure 06_01 #
 ################
-png("C:\\Data\\4367OS_06_01.png",width=5.5,height=3.45,units="in",res=300,pointsize=10)
+#png("./Data files/4367OS_06_01.png",width=5.5,height=3.45,units="in",res=300,pointsize=10)
 plot(dem)
 plot(haifa_surrounding, add = TRUE)
 dev.off()
@@ -41,7 +41,7 @@ dem = crop(dem, haifa_surrounding)
 ################
 # Figure 06_02 #
 ################
-png("C:\\Data\\4367OS_06_02.png",width=5.0,height=5.5,units="in",res=300)
+#png("./Data files/4367OS_06_02.png",width=5.0,height=5.5,units="in",res=300)
 plot(dem)
 dev.off()
 
@@ -51,7 +51,7 @@ dem_agg16 = aggregate(dem, fact = 16)
 ################
 # Figure 06_03 #
 ################
-png("C:\\Data\\4367OS_06_03.png",width=5.5,height=2.0,units="in",res=300,pointsize=10)
+#png("./Data files/4367OS_06_03.png",width=5.5,height=2.0,units="in",res=300,pointsize=10)
 par(mfrow = c(1,3), mai = rep(0.5, 4))
 plot(dem, main = "Original image")
 plot(dem_agg8, main = "8x8 aggregated")
@@ -67,10 +67,10 @@ l_resample = resample(ndvi_00, r[[l_date]], method = "ngb")
 ################
 # Figure 06_04 #
 ################
-png("C:\\Data\\4367OS_06_04.png",width=5.5,height=3.0,units="in",res=300,pointsize=10)
+#png("./Data files/4367OS_06_04.png",width=5.5,height=3.0,units="in",res=300,pointsize=10)
 par(mfrow=c(1,2), mai = rep(0.75, 4))
 plot(ndvi_00, main = "Original Landsat image")
-plot(l_resample, ext = extent(ndvi_00), 
+plot(l_resample, ext = extent(ndvi_00),
 	main = "Resampled to MODIS")
 dev.off()
 
@@ -81,7 +81,7 @@ r_resample = extend(r_resample, r[[l_date]])
 ################
 # Figure 06_05 #
 ################
-png("C:\\Data\\4367OS_06_05.png",width=5.5,height=3.0,units="in",res=300,pointsize=10)
+#png("./Data files/4367OS_06_05.png",width=5.5,height=3.0,units="in",res=300,pointsize=10)
 par(mfrow=c(1,2), mai = rep(0.75, 4))
 plot(r[[l_date]], main = "Original MODIS image")
 plot(extent(ndvi_00), add = TRUE)
@@ -89,27 +89,27 @@ plot(r_resample, main = "Resampled to Landsat")
 dev.off()
 
 r_resample_ngb = resample(r[[l_date]], ndvi_00, method = "ngb")
-r_resample_bil = resample(r[[l_date]], ndvi_00, 
+r_resample_bil = resample(r[[l_date]], ndvi_00,
 	method = "bilinear")
 resample_results = stack(r_resample_ngb, r_resample_bil)
-names(resample_results) = c("Nearest neighbor", 
+names(resample_results) = c("Nearest neighbor",
 	"Bilinear interpolation")
 
 ################
 # Figure 06_06 #
 ################
-png("C:\\Data\\4367OS_06_06.png",width=5.5,height=3.5,units="in",res=300)
+#png("./Data files/4367OS_06_06.png",width=5.5,height=3.5,units="in",res=300)
 library(rasterVis)
-levelplot(resample_results, 
-	par.settings = RdBuTheme, 
+levelplot(resample_results,
+	par.settings = RdBuTheme,
 	contour = TRUE)
 dev.off()
 
 dem
 
-dem = projectRaster(from = dem, 
-	crs = proj4string(r), 
-	method = "ngb", 
+dem = projectRaster(from = dem,
+	crs = proj4string(r),
+	method = "ngb",
 	res = 90)
 
 dem
@@ -117,14 +117,14 @@ dem
 ################
 # Figure 06_07 #
 ################
-png("C:\\Data\\4367OS_06_07.png",width=5.0,height=5.5,units="in",res=300)
+#png("./Data files/4367OS_06_07.png",width=5.0,height=5.5,units="in",res=300)
 plot(dem)
 dev.off()
 
 matrix(1, nrow = 3, ncol = 3)
 
-l_rec_focal = focal(l_rec, 
-	w = matrix(1, nrow = 3, ncol = 3), 
+l_rec_focal = focal(l_rec,
+	w = matrix(1, nrow = 3, ncol = 3),
 	fun = max)
 
 l_rec_focal_clump = clump(l_rec_focal, gaps = FALSE)
@@ -132,7 +132,7 @@ l_rec_focal_clump = clump(l_rec_focal, gaps = FALSE)
 ################
 # Figure 06_08 #
 ################
-png("C:\\Data\\4367OS_06_08.png",width=5.5,height=2.0,units="in",res=300,pointsize=10)
+#png("./Data files/4367OS_06_08.png",width=5.5,height=2.0,units="in",res=300,pointsize=10)
 par(mfrow = c(1,3), mai = rep(0.5, 4))
 plot(l_rec, main = "Original image")
 plot(l_rec_focal, main = "Filtered")
@@ -145,7 +145,7 @@ aspect = terrain(dem, "aspect")
 ################
 # Figure 06_09 #
 ################
-png("C:\\Data\\4367OS_06_09.png",width=5.5,height=2.5,units="in",res=300,pointsize=10)
+#png("./Data files/4367OS_06_09.png",width=5.5,height=2.5,units="in",res=300,pointsize=10)
 plot(stack(slope, aspect))
 dev.off()
 
@@ -154,7 +154,7 @@ hill = hillShade(slope, aspect, 20, 235)
 ################
 # Figure 06_10 #
 ################
-png("C:\\Data\\4367OS_06_10.png",width=5.5,height=5.5,units="in",res=300)
+#png("./Data files/4367OS_06_10.png",width=5.5,height=5.5,units="in",res=300)
 levelplot(hill, par.settings = GrTheme, margin = FALSE)
 dev.off()
 
@@ -172,13 +172,13 @@ names(season_means) = seasons
 ################
 # Figure 06_11 #
 ################
-png("C:\\Data\\4367OS_06_11.png",width=5.5,height=5.5,units="in",res=300)
+#png("./Data files/4367OS_06_11.png",width=5.5,height=5.5,units="in",res=300)
 levelplot(season_means, par.settings=RdBuTheme, contour=TRUE)
 dev.off()
 
-month_means = stackApply(r, 
-	indices = dates$month, 
-	fun = mean, 
+month_means = stackApply(r,
+	indices = dates$month,
+	fun = mean,
 	na.rm = TRUE)
 
 names(month_means) = month.abb
@@ -186,7 +186,7 @@ names(month_means) = month.abb
 ################
 # Figure 06_12 #
 ################
-png("C:\\Data\\4367OS_06_12.png",width=5.5,height=4.5,units="in",res=300)
+#png("./Data files/4367OS_06_12.png",width=5.5,height=4.5,units="in",res=300)
 levelplot(month_means, par.settings = RdBuTheme, contour = TRUE)
 dev.off()
 
@@ -211,7 +211,7 @@ for(i in 1:nlayers(r)) {
 ################
 # Figure 06_14 #
 ################
-png("C:\\Data\\4367OS_06_14.png",width=5.5,height=2.5,units="in",res=300)
+#png("./Data files/4367OS_06_14.png",width=5.5,height=2.5,units="in",res=300)
 levelplot(s,
 	par.settings = RdBuTheme,
 	contour = TRUE,
